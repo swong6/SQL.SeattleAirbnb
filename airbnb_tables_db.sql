b35790400f385bf2e4aba6aa4495e2fa02498edd
Sharmaine Wong <99834601+swong6@users.noreply.github.com>
2024-08-01 01:05:40 -0700

    Add files via upload

---
+++ airbnb_tables_db.sql
@@ -0,0 +1,74 @@
+/* 
+Script to Create and Populate Seattle Airbnb Database
+This script creates a database for Seattle Airbnb data, defines tables, and loads data from CSV files.
+*/
+-- Create database
+CREATE DATABASE airbnb;
+USE airbnb;
+
+-- Create listings table
+CREATE TABLE listings (
+	id INT PRIMARY KEY,
+	name VARCHAR(255),
+	host_ID INT,
+    host_name VARCHAR(255),
+    neighborhood VARCHAR(255),
+    latitude FLOAT,
+    longitude FLOAT,
+    room_type VARCHAR(255),
+    price INT,
+    minimum_nights INT,
+    number_of_reviews INT,
+    last_review DATE,
+    reviews_per_month FLOAT,
+    calculated_host_listings_count INT,
+    availability_365 INT
+);
+
+-- Create calendar table
+CREATE TABLE calendar (
+	listing_id INT,
+	date DATE,
+	available VARCHAR(5),
+    price VARCHAR(50),
+    PRIMARY KEY (listing_id, date)   
+);
+
+-- Create reviews table
+CREATE TABLE reviews (
+	listing_id INT,
+    id INT PRIMARY KEY,
+	date DATE,
+	reviewer_id INT,
+    reviewer_name VARCHAR(255),
+    comments TEXT   
+);
+
+-- Load data into tables
+LOAD DATA LOCAL INFILE 'C:/Users/sharm/Documents/Seattle Airbnb Open Data/calendar.csv'
+INTO TABLE calendar
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/Users/sharm/Documents/Seattle Airbnb Open Data/listings.csv'
+INTO TABLE listings
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'C:/Users/sharm/Documents/Seattle Airbnb Open Data/reviews.csv'
+INTO TABLE reviews
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+
+-- Verify data
+SELECT * FROM reviews LIMIT 50;
+/* 
+-----------------------------------Loading CSV Completed-------------------------------------------
+*/
